1c6d518d99a3cad13cc8833078c68e13537845d2
AomSirawit <makdeesirawit@gmail.com>
2023-05-24 19:57:50 +0700

    ีupdate
    update

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
 
-app.get("/", (req,res) =>{
-    
-    res.send("Hello");
+app.set("views","./src/views");
+app.set("view engine", "ejs");
 
+app.get("/",(req,res) => {
+
+    res.render('index',{username: 'AomZa55+',customers: ["kitti", "kittikorn" , "kitty"]});
 })
 
-app.listen(port, () =>{
-    debug("Listening on port" + chalk.green(port));
+app.listen(PORT, () =>{
+    debug("Listening on port : " + chalk.green(PORT));
 })
